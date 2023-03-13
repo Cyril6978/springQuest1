39598a43281e5c14c994b1863a92e8dec74bbc37
DAVID Cyril (SNCF VOYAGEURS / DIRECTION DE LIGNES CITI LYON / TLPR SIEGE) <7807880b@commun.ad.sncf.fr>
2023-03-13 14:17:06 +0100

    first commit

---
+++ src/test/java/com/quest1/springQuest1/SpringQuest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quest1.springQuest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
